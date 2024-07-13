03902551ad9e12e4291dd8ceef55105a1a984a99
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2024-07-13 21:58:00 +0100

    make detach button open... something

--- src/sheet.ts
+++ src/sheet.ts
@@ -4,16 +4,27 @@ class SheetHandler {
   init() {
     Hooks.on(
       'getActorSheetHeaderButtons',
-      (_sheet: ActorSheet, buttons: Application.HeaderButton[]) => {
+      (sheet: ActorSheet, buttons: Application.HeaderButton[]) => {
         const button: Application.HeaderButton = {
           label: 'SHEET-O-SCOPE.detach',
           class: 'sheet-detach',
-          icon: 'fa-solid fa-arrow-up-right-from-square',
-          onclick: null
+          icon: 'fa-arrow-up-right-from-square',
+          onclick: () => {
+            this.handleSheetDetach(sheet);
+          }
         };
 
         buttons.unshift(button);
       }
+    );
+  }
+
+  handleSheetDetach(sheet: ActorSheet) {
+    const { id, width, height } = sheet.options;
+
+    window.open(`/game?sheetView=${id}&width=${width}&height=${height}`,
+      '_blank',
+      `popup=true,width=${width},height=${height}`
     );
   }
 }
