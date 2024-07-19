dc2a45c1e5d09c258485b9bda23b7a13e5a87c70
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2024-07-19 10:28:06 +0100

    add basic css infrastructure

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { checker } from 'vite-plugin-checker';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   build: {
@@ -16,6 +17,16 @@ export default defineConfig({
   plugins: [
     checker({
       typescript: true
+    }),
+    // caveat: during --watch, changes to this file don't get copied over
+    // probably not worth addressing unless this became a more css-heavy project
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'styles/*',
+          dest: ''
+        }
+      ]
     })
   ]
 });
