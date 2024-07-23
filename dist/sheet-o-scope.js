05952723d01a31596ebdf2c28b8ae1a09a7c1e6d
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2024-07-23 19:21:22 +0100

    build

--- dist/sheet-o-scope.js
+++ dist/sheet-o-scope.js
@@ -1,16 +1,16 @@
-var _ = Object.defineProperty;
-var M = (t) => {
-  throw TypeError(t);
+var F = Object.defineProperty;
+var M = (s) => {
+  throw TypeError(s);
 };
-var K = (t, s, e) => s in t ? _(t, s, { enumerable: !0, configurable: !0, writable: !0, value: e }) : t[s] = e;
-var u = (t, s, e) => K(t, typeof s != "symbol" ? s + "" : s, e), C = (t, s, e) => s.has(t) || M("Cannot " + e);
-var g = (t, s, e) => (C(t, s, "read from private field"), e ? e.call(t) : s.get(t)), d = (t, s, e) => s.has(t) ? M("Cannot add the same private member more than once") : s instanceof WeakSet ? s.add(t) : s.set(t, e), S = (t, s, e, n) => (C(t, s, "write to private field"), n ? n.call(t, e) : s.set(t, e), e), i = (t, s, e) => (C(t, s, "access private method"), e);
-var w = /* @__PURE__ */ ((t) => (t.Main = "main", t.PopUp = "popUp", t))(w || {}), h = /* @__PURE__ */ ((t) => (t.Actor = "actor", t.Item = "item", t.Journal = "journal", t))(h || {});
-function Q(t) {
-  return new URL(t).searchParams.get("sheetView") ? w.PopUp : w.Main;
+var _ = (s, t, e) => t in s ? F(s, t, { enumerable: !0, configurable: !0, writable: !0, value: e }) : s[t] = e;
+var d = (s, t, e) => _(s, typeof t != "symbol" ? t + "" : t, e), E = (s, t, e) => t.has(s) || M("Cannot " + e);
+var g = (s, t, e) => (E(s, t, "read from private field"), e ? e.call(s) : t.get(s)), u = (s, t, e) => t.has(s) ? M("Cannot add the same private member more than once") : t instanceof WeakSet ? t.add(s) : t.set(s, e), S = (s, t, e, n) => (E(s, t, "write to private field"), n ? n.call(s, e) : t.set(s, e), e), i = (s, t, e) => (E(s, t, "access private method"), e);
+var m = /* @__PURE__ */ ((s) => (s.Main = "main", s.PopUp = "popUp", s))(m || {}), h = /* @__PURE__ */ ((s) => (s.Actor = "actor", s.Item = "item", s.Journal = "journal", s))(h || {});
+function K(s) {
+  return new URL(s).searchParams.get("sheetView") ? m.PopUp : m.Main;
 }
-function X(t) {
-  const e = new URL(t).searchParams;
+function Q(s) {
+  const e = new URL(s).searchParams;
   if (e.get("sheetView")) {
     const n = e.get("id"), o = e.get("type");
     if (n && o && // apparently the best way to check an enum has a value
@@ -19,32 +19,26 @@ function X(t) {
   }
   return null;
 }
-function $(t) {
-  console.log(`sheet-o-scope | ${t}`);
-}
-function Y(t) {
-  console.warn(`sheet-o-scope | ${t}`);
-}
 function A() {
   return game;
 }
-function I(t, s) {
-  var n, o, r, l, k, U;
+function R(s, t) {
+  var n, o, r, l, k, C;
   const e = A();
-  if (s === h.Actor)
-    return (o = (n = e.actors) == null ? void 0 : n.get(t)) == null ? void 0 : o.sheet;
-  if (s === h.Item)
-    return (l = (r = e.items) == null ? void 0 : r.get(t)) == null ? void 0 : l.sheet;
-  if (s === h.Journal)
-    return (U = (k = e.journal) == null ? void 0 : k.get(t)) == null ? void 0 : U.sheet;
+  if (t === h.Actor)
+    return (o = (n = e.actors) == null ? void 0 : n.get(s)) == null ? void 0 : o.sheet;
+  if (t === h.Item)
+    return (l = (r = e.items) == null ? void 0 : r.get(s)) == null ? void 0 : l.sheet;
+  if (t === h.Journal)
+    return (C = (k = e.journal) == null ? void 0 : k.get(s)) == null ? void 0 : C.sheet;
 }
-var E = /* @__PURE__ */ ((t) => (t.Reattach = "reattach", t))(E || {}), p, f, O, J;
-class R extends EventTarget {
+var U = /* @__PURE__ */ ((s) => (s.Reattach = "reattach", s))(U || {}), p, f, J, L;
+class I extends EventTarget {
   constructor(e = null) {
     super();
-    d(this, f);
-    d(this, p);
-    S(this, p, e), window.addEventListener("message", i(this, f, O).bind(this));
+    u(this, f);
+    u(this, p);
+    S(this, p, e), window.addEventListener("message", i(this, f, J).bind(this));
   }
   send(e, n) {
     const o = {
@@ -55,33 +49,33 @@ class R extends EventTarget {
     g(this, p) && g(this, p).postMessage(o);
   }
 }
-p = new WeakMap(), f = new WeakSet(), O = function(e) {
-  if (i(this, f, J).call(this, e)) {
+p = new WeakMap(), f = new WeakSet(), J = function(e) {
+  if (i(this, f, L).call(this, e)) {
     const n = new MessageEvent("message", { data: e.data });
     this.dispatchEvent(n);
   }
-}, J = function(e) {
+}, L = function(e) {
   const n = e.data;
   return e.origin === window.location.origin && n.sender === "sheet-o-scope";
 };
-class Z {
+class X {
   constructor() {
-    u(this, "label", "SHEET-O-SCOPE.detach");
-    u(this, "class", "sheet-detach");
-    u(this, "icon", "fa-solid fa-arrow-right-from-bracket");
-    u(this, "onclick", null);
+    d(this, "label", "SHEET-O-SCOPE.detach");
+    d(this, "class", "sheet-detach");
+    d(this, "icon", "fa-solid fa-arrow-right-from-bracket");
+    d(this, "onclick", null);
   }
 }
-var m, a, L, z, v, D, N;
-class ee extends EventTarget {
+var w, a, z, O, v, T, j;
+class Y extends EventTarget {
   constructor() {
     super();
-    d(this, a);
-    d(this, m);
-    S(this, m, new R()), Hooks.once("ready", i(this, a, L).bind(this));
+    u(this, a);
+    u(this, w);
+    S(this, w, new I()), Hooks.once("ready", i(this, a, z).bind(this));
   }
 }
-m = new WeakMap(), a = new WeakSet(), L = function() {
+w = new WeakMap(), a = new WeakSet(), z = function() {
   var n, o, r;
   const e = A();
   if (!((n = e.modules.get("lib-wrapper")) != null && n.active) && ((o = e.user) != null && o.isGM)) {
@@ -90,7 +84,7 @@ m = new WeakMap(), a = new WeakSet(), L = function() {
     );
     return;
   }
-  $("Setting up changes to main window"), Hooks.on(
+  Hooks.on(
     "getActorSheetHeaderButtons",
     i(this, a, v).bind(this, h.Actor)
   ), Hooks.on(
@@ -99,76 +93,76 @@ m = new WeakMap(), a = new WeakSet(), L = function() {
   ), Hooks.on(
     "getJournalSheetHeaderButtons",
     i(this, a, v).bind(this, h.Journal)
-  ), g(this, m).addEventListener(
+  ), g(this, w).addEventListener(
     "message",
-    i(this, a, z).bind(this)
+    i(this, a, O).bind(this)
   );
-}, z = function(e) {
+}, O = function(e) {
   const n = e.data;
-  n.action === E.Reattach && i(this, a, N).call(this, n.data);
+  n.action === U.Reattach && i(this, a, j).call(this, n.data);
 }, v = function(e, n, o) {
-  const r = new Z();
+  const r = new X();
   r.onclick = () => {
-    i(this, a, D).call(this, e, n);
+    i(this, a, T).call(this, e, n);
   }, o.unshift(r);
-}, D = function(e, n) {
+}, T = function(e, n) {
   const { width: o, height: r } = n.options, l = n.document.id;
   n.close(), window.open(
     `/game?sheetView=1&id=${l}&type=${e}`,
     "_blank",
     `popup=true,width=${o},height=${r}`
   );
-}, N = function(e) {
-  const { id: n, type: o } = e, r = I(n, o);
+}, j = function(e) {
+  const { id: n, type: o } = e, r = R(n, o);
   r && r.render(!0);
 };
-class te {
+class Z {
   constructor() {
-    u(this, "label", "SHEET-O-SCOPE.reattach");
-    u(this, "class", "sheet-reattach");
-    u(this, "icon", "fa-solid fa-arrow-right-to-bracket");
-    u(this, "onclick", null);
+    d(this, "label", "SHEET-O-SCOPE.reattach");
+    d(this, "class", "sheet-reattach");
+    d(this, "icon", "fa-solid fa-arrow-right-to-bracket");
+    d(this, "onclick", null);
   }
 }
-var H, T;
-class se {
+var H, N;
+class ee {
   constructor() {
-    d(this, H);
+    u(this, H);
   }
   run() {
-    const s = i(this, H, T);
+    const t = i(this, H, N);
     libWrapper.register(
       "sheet-o-scope",
       "Notifications.prototype.notify",
       function(e, ...n) {
-        return s(n) ? -1 : e(...n);
+        return t(n) ? -1 : e(...n);
       }
     );
   }
 }
-H = new WeakSet(), T = function(s) {
-  const [e, n] = s;
+H = new WeakSet(), N = function(t) {
+  const [e, n] = t;
   return !!(n === "info" && e.includes("not displayed because the game Canvas is disabled") || n === "error" && e.includes(
     "Foundry Virtual Tabletop requires a minimum screen resolution"
   ));
 };
-var y, j;
-class ne {
+var y, V;
+class te {
   constructor() {
-    d(this, y);
+    u(this, y);
   }
   run() {
-    const s = i(this, y, j);
+    const t = i(this, y, V);
     libWrapper.register(
       "sheet-o-scope",
       "Application.prototype.render",
       function(e, ...n) {
-        return s(this) ? this : e(...n);
+        return t(this) ? this : e(...n);
       }
     );
   }
 }
-y = new WeakSet(), j = function(s) {
+y = new WeakSet(), V = function(t) {
   return !![
     "navigation",
     "sidebar",
@@ -176,31 +170,31 @@ y = new WeakSet(), j = function(s) {
     "hotbar",
     "pause",
     "controls"
-  ].includes(s.options.id);
+  ].includes(t.options.id);
 };
-class oe {
+class se {
   run() {
     libWrapper.register(
       "sheet-o-scope",
       "ClientSettings.prototype.get",
-      function(s, ...e) {
-        return e.join(".") === "core.noCanvas" ? !0 : s(...e);
+      function(t, ...e) {
+        return e.join(".") === "core.noCanvas" ? !0 : t(...e);
       }
     );
   }
 }
-const ie = [
-  se,
-  ne,
-  oe
+const ne = [
+  ee,
+  te,
+  se
 ];
-var b, c, V, q, G, B, x;
-class re {
-  constructor(s) {
-    d(this, c);
-    d(this, b);
+var b, c, $, q, D, B, x;
+class oe {
+  constructor(t) {
+    u(this, c);
+    u(this, b);
     var e;
-    S(this, b, new R(window.opener)), Hooks.once("init", i(this, c, V).bind(this)), Hooks.once("ready", i(this, c, q).bind(this, s)), Hooks.on(
+    S(this, b, new I(window.opener)), Hooks.once("init", i(this, c, $).bind(this)), Hooks.once("ready", i(this, c, q).bind(this, t)), Hooks.on(
       "getActorSheetHeaderButtons",
       i(this, c, B).bind(this, h.Actor)
     ), Hooks.on(
@@ -212,22 +206,22 @@ class re {
     ), (e = document.querySelector("body")) == null || e.classList.add("sheet-o-scope-popup");
   }
 }
-b = new WeakMap(), c = new WeakSet(), V = function() {
-  ie.forEach((s) => {
-    new s().run();
+b = new WeakMap(), c = new WeakSet(), $ = function() {
+  ne.forEach((t) => {
+    new t().run();
   });
-}, q = function(s) {
-  const { id: e, type: n } = s, o = I(e, n);
-  o ? ($(`Opening sheet for ${n} with ID: ${e}`), o.render(!0, {
+}, q = function(t) {
+  const { id: e, type: n } = t, o = R(e, n);
+  o && o.render(!0, {
     minimizable: !1,
     resizable: !1
-  })) : Y(`Couldn't find sheet for ${n} with ID: ${e}`), window.addEventListener("resize", i(this, c, G).bind(this, o));
-}, G = function(s) {
-  s && s.setPosition({
+  }), window.addEventListener("resize", i(this, c, D).bind(this, o));
+}, D = function(t) {
+  t && t.setPosition({
     width: window.innerWidth,
     height: window.innerHeight
   });
-}, B = function(s, e, n) {
+}, B = function(t, e, n) {
   const o = e.document.id;
   if (!o)
     return;
@@ -235,13 +229,12 @@ b = new WeakMap(), c = new WeakSet(), V = function() {
   r && (r.onclick = () => {
     window.close();
   });
-  const l = new te();
+  const l = new Z();
   l.onclick = () => {
-    i(this, c, x).call(this, s, o);
+    i(this, c, x).call(this, t, o);
   }, n.unshift(l);
-}, x = function(s, e) {
-  g(this, b).send(E.Reattach, { id: e, type: s }), window.close();
+}, x = function(t, e) {
+  g(this, b).send(U.Reattach, { id: e, type: t }), window.close();
 };
-const F = window.location.toString(), P = Q(F), W = X(F);
-P === w.Main ? new ee() : P === w.PopUp && W && new re(W);
-CONFIG.debug.hooks = !0;
+const G = window.location.toString(), P = K(G), W = Q(G);
+P === m.Main ? new Y() : P === m.PopUp && W && new oe(W);
