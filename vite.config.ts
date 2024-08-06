03cc23909a21304e955bc98e0393f058d672a5e4
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2024-08-06 18:21:27 +0100

    update manifest / download URLs based on build so people can download the latest dev build

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,20 @@ import zipPack from 'vite-plugin-zip-pack';
 
 import moduleJson from './static/module.json';
 
+const devReleaseUrlBase = 'https://raw.githubusercontent.com/andrey-p/fvtt-sheet-o-scope/main/dist';
+const versionedReleaseUrlBase = 'https://github.com/andrey-p/fvtt-sheet-o-scope/releases';
+
 export default defineConfig(({ mode }) => {
   let outDir: string;
   let createZip: boolean = false;
-  const version = moduleJson.version as string;
+
+  // in dev / unversioned builds, the manifest and download
+  // properties can be anything
+  let transformHandler = (moduleJsonString) => {
+    return moduleJsonString
+      .replace('{{manifestLocation}}', devReleaseUrlBase)
+      .replace('{{zipLocation}}', devReleaseUrlBase);
+  };
 
   if (mode === 'local') {
     outDir = 'build';
@@ -17,8 +27,21 @@ export default defineConfig(({ mode }) => {
     outDir = 'dist';
     createZip = true;
   } else if (mode === 'versioned-release') {
+    const version = moduleJson.version as string;
+
     outDir = `build/${version}`;
     createZip = true;
+
+    // in versioned releases:
+    transformHandler = (moduleJsonString) => {
+      return moduleJsonString
+        // the manifest needs to be the _latest_ one
+        // (so Foundry can check it and figure out if there's an update)
+        .replace('{{manifestLocation}}', `${versionedReleaseUrlBase}/latest/download`)
+        // the download needs to match the one of the _current version_
+        // (so Foundry has a consistent path to it)
+        .replace('{{zipLocation}}', `${versionedReleaseUrlBase}/download/${version}`);
+    };
   }
 
   const plugins = [
@@ -34,8 +57,16 @@ export default defineConfig(({ mode }) => {
           dest: ''
         },
         {
-          src: 'static/*',
+          src: 'static/lang',
           dest: ''
+        },
+        {
+          src: 'static/module.json',
+          dest: '',
+          transform: {
+            encoding: 'utf8',
+            handler: transformHandler
+          }
         }
       ]
     })
