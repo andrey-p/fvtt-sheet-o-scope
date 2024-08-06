d17222e50442071db5037ebcc6ef35f16335b272
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2024-08-06 11:42:24 +0100

    rejig scripts so zips can be generated in a programmatically chosen folder for release

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,51 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { checker } from 'vite-plugin-checker';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import zipPack from 'vite-plugin-zip-pack';
+
+import moduleJson from './static/module.json';
 
 export default defineConfig(({ mode }) => {
   let outDir: string;
+  let createZip: boolean = false;
+  const version = moduleJson.version as string;
 
-  if (mode === 'development') {
+  if (mode === 'local') {
     outDir = 'build';
-  } else {
+  } else if (mode === 'dev-release') {
     outDir = 'dist';
+    createZip = true;
+  } else if (mode === 'versioned-release') {
+    outDir = `build/${version}`;
+    createZip = true;
+  }
+
+  const plugins = [
+    checker({
+      typescript: true
+    }),
+    // caveat: during --watch, changes to this file don't get copied over
+    // probably not worth addressing unless this became a more css-heavy project
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'styles/*',
+          dest: ''
+        },
+        {
+          src: 'static/*',
+          dest: ''
+        }
+      ]
+    })
+  ];
+
+  if (createZip) {
+    plugins.push(zipPack({
+      inDir: outDir,
+      outDir,
+      outFileName: 'sheet-o-scope.zip'
+    }));
   }
 
   return {
@@ -25,24 +62,6 @@ export default defineConfig(({ mode }) => {
     test: {
       include: ['./tests/**/*.ts']
     },
-    plugins: [
-      checker({
-        typescript: true
-      }),
-      // caveat: during --watch, changes to this file don't get copied over
-      // probably not worth addressing unless this became a more css-heavy project
-      viteStaticCopy({
-        targets: [
-          {
-            src: 'styles/*',
-            dest: ''
-          },
-          {
-            src: 'static/*',
-            dest: ''
-          }
-        ]
-      })
-    ]
+    plugins
   }
 });
