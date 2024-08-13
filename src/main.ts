9d124cdc5e5686f3c8ec5f14478f8719393de9ab
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2024-08-13 18:32:13 +0100

    check next openable popup once popup window is ready

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { getWindowMode } from './utils/url';
-import { getNextOpenablePopUp } from './utils/storage';
 
 import WindowMode from './enums/window-mode';
 
@@ -8,15 +7,14 @@ import PopUpWindow from './popup-window';
 
 const url = window.location.toString();
 const windowMode = getWindowMode(url);
-const popUpConfig = getNextOpenablePopUp();
 
 // this module has two entry points, one for the main window
 // and one for the popup that this module opens
 if (windowMode === WindowMode.Main) {
   // the main window mostly just adds a detach button to the sheets
   new MainWindow();
-} else if (windowMode === WindowMode.PopUp && popUpConfig) {
+} else if (windowMode === WindowMode.PopUp) {
   // the popup window is a bit heavier - it shows the sheet in question
   // but also attempts to strip away lots of foundry UI that we don't need
-  new PopUpWindow(popUpConfig);
+  new PopUpWindow();
 }
